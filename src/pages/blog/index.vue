d41e08aff210d410d058bb249902df9213949002
dai <chamestica@gmail.com>
2020-06-11 17:43:53 +0900

    Add blog post list to blog page

--- src/pages/blog/index.vue
+++ src/pages/blog/index.vue
@@ -1,29 +1,48 @@
 <template>
   <div>
     <h1 class="title">
-      {{ content.title.ja_jp }}
+      {{ pageContent.title.ja_jp }}
     </h1>
+    <ul>
+      <NuxtLink
+        v-for="postContent of postContentList"
+        :key="postContent.id"
+        :to="`/blog/${postContent.id}`"
+        tag="li"
+        class="item"
+      >
+        <h2>{{ postContent.title.ja_jp }}</h2>
+      </NuxtLink>
+    </ul>
   </div>
 </template>
 
 <script>
-import { getContent } from '~/assets/js/pages-fetcher'
+import { getContent as getPageContent } from '~/assets/js/pages-fetcher'
+import { getContentList as getPostContentList } from '~/assets/js/posts-fetcher'
 import { create as createHead } from '~/assets/js/head-creator'
 
 export default {
   async asyncData({ route }) {
     return {
-      content: await getContent(route.name)
+      pageContent: await getPageContent(route.name),
+      postContentList: await getPostContentList()
     }
   },
 
   head() {
     return createHead(
-      `${this.content.title.ja_jp} | ${this.$siteData.title.ja_jp}`,
-      this.content.description.ja_jp,
+      `${this.pageContent.title.ja_jp} | ${this.$siteData.title.ja_jp}`,
+      this.pageContent.description.ja_jp,
       this.$siteData.ogImage.url,
       `${process.env.BASE_URL}${this.$route.path}`
     )
   }
 }
 </script>
+
+<style scoped>
+.item {
+  cursor: pointer;
+}
+</style>
