87460b46a7074415ad85b2511e92295a598eef94
dai <chamestica@gmail.com>
2020-06-26 03:37:59 +0900

    Remove unused function

--- src/assets/js/input-fields-fetcher.js
+++ src/assets/js/input-fields-fetcher.js
@@ -2,22 +2,7 @@ import axios from 'axios'
 import { inputFieldsPerRequest } from '../json/variables'
 import { createInputFieldsFetcherConfig } from './fetcher-config-creator'
 
-export async function getCInputFieldContent(id) {
-  if (!id) {
-    const error = new Error('Page ID to request is missing.')
-    return Promise.reject(error)
-  }
-  const config = createInputFieldsFetcherConfig()
-  const response = await axios.get(id, config)
-  if (!response.data) throw new Error('API response is invalid.')
-  return response.data
-}
-
-export async function getCInputFieldContentList({
-  fields,
-  offset,
-  limit
-} = {}) {
+export async function getInputFieldContentList({ fields, offset, limit } = {}) {
   const config = createInputFieldsFetcherConfig({ fields, offset, limit })
   const response = await axios.get('', config)
   if (!response.data) throw new Error('API response is invalid.')
@@ -30,7 +15,7 @@ export async function getAllInputFieldContents() {
   do {
     const offset = allInputFieldContents.length
     const options = { limit: inputFieldsPerRequest, offset }
-    inputFieldContentList = await getCInputFieldContentList(options)
+    inputFieldContentList = await getInputFieldContentList(options)
     allInputFieldContents.push(...inputFieldContentList)
   } while (inputFieldContentList.length === inputFieldsPerRequest)
   return allInputFieldContents
